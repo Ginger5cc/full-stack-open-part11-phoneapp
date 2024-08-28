247cac77c5ec5324743df480c42705ec873aa9b2
Ginger <gingerginger5cc@gmail.com>
2024-08-28 10:12:06 -0400

    try the test workflow 5th time

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,6 +3,8 @@ const { test, expect } = require('@playwright/test')
 
 test('Frontpage has title', async ({ page }) => {
   await page.goto('http://localhost:5173/')
+  const content =await page.content()
+  console.log('content is', content)
   await expect(page.getByText('Phonebook')).toBeVisible()
   await expect(page.getByText('All Numbers')).toBeVisible()
 })
